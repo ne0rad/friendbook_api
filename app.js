300e64333c2485cc88002c6fa5ab3fda9ec9900e
ne0rad <usackasr@gmail.com>
2022-01-18 12:02:15 +0000

    Added MongoDB connection

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
 var express = require('express');
+require('dotenv').config();
 var path = require('path');
 var logger = require('morgan');
 var cors = require('cors');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+// MongoDB connection
+const DEV_DB_URI = "mongodb://127.0.0.1:27017/friendbook";
+mongoose.connect(DEV_DB_URI, { useNewUrlParser: true });
 
 var app = express();
 
 var corsOptions = {
     origin: 'http://localhost:3000',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+    optionsSuccessStatus: 200
 }
 
+
+
 app.use(logger('dev'));
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', cors(corsOptions), indexRouter);
-app.use('/users', cors(corsOptions), usersRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 module.exports = app;
