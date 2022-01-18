a9a328d6d50dc1cf135c597516c83031f3e82aa5
ne0rad <usackasr@gmail.com>
2022-01-18 14:03:43 +0000

    Added user creation

--- controllers/user.js
+++ controllers/user.js
@@ -1,18 +1,48 @@
-exports.createUser = function (req, res, next) {
-    if (!req.body.username.trim()) {
-        res.sendStatus(406).send({ error: "username", message: "Username can't be empty." });
-        return;
+const User = require('../models/user');
+const { body, validationResult } = require('express-validator');
+
+exports.createUser = [
+
+    body('username')
+        .trim()
+        .isLength({ min: 3, max: 50 })
+        .withMessage('Username too short.')
+        .bail()
+        .isAlphanumeric()
+        .withMessage('Only letters and numbers.')
+        .bail()
+        .escape()
+        .custom(async function (value) {
+            var existingUser = await User.exists({ username: value });
+            if (existingUser === true) return Promise.reject();
+            else return Promise.resolve();
+        })
+        .withMessage('Username already taken.'),
+
+    body('password')
+        .isLength({ min: 3, max: 50 })
+        .withMessage('Password too short.')
+        .escape(),
+
+    (req, res, next) => {
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+
+            // Got errors, send them back to frontend
+            let error = errors.array()[0];
+            console.log(error);
+            res.status(406).send({ error: error.param, message: error.msg });
+            return;
+
+        } else {
+
+            const user = User({
+                username: req.body.username,
+                password: req.body.password,
+                role: 0
+            });
+            user.save();
+        }
     }
-    if (!req.body.password.trim()) {
-        res.sendStatus(406).send({ error: "password", message: "Password can't be empty" });
-    }
-    if (req.body.username === "qwe") {
-        res.status(406).send({ error: "username", message: "Username already taken." });
-        return;
-    }
-    const user = {
-        username: req.body.username,
-        password: req.body.password
-    }
-    res.send(user);
-}
+]
