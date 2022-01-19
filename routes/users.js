cb7e6b814fe2d1e704fbf598ac7305e92e11da0f
ne0rad <usackasr@gmail.com>
2022-01-19 11:31:51 +0000

    Added loginUser route

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
-const { createUser } = require('../controllers/user');
+const { createUser, loginUser } = require('../controllers/user');
 
 router.post('/createUser', createUser);
+router.post('/loginUser', loginUser);
 
 module.exports = router;
