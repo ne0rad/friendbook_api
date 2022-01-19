bd025a28ca6d5ba52b51abe7f1b37dd70ce0d0cc
ne0rad <usackasr@gmail.com>
2022-01-19 11:31:43 +0000

    Added login

--- controllers/user.js
+++ controllers/user.js
@@ -1,27 +1,34 @@
 const User = require('../models/user');
 const { body, validationResult } = require('express-validator');
+const { sendStatus } = require('express/lib/response');
 
 exports.createUser = [
 
     body('username')
         .trim()
-        .isLength({ min: 3, max: 50 })
+        .isLength({ min: 3 })
         .withMessage('Username too short.')
+        .bail()
+        .isLength({ max: 50 })
+        .withMessage('Username too long.')
         .bail()
         .isAlphanumeric()
         .withMessage('Only letters and numbers.')
         .bail()
         .escape()
         .custom(async function (value) {
-            var existingUser = await User.exists({ username: value });
-            if (existingUser === true) return Promise.reject();
+            const existingUser = await User.exists({ username: { "$regex": value, "$options": "i" } });
+            if (existingUser) return Promise.reject();
             else return Promise.resolve();
         })
         .withMessage('Username already taken.'),
 
     body('password')
-        .isLength({ min: 3, max: 50 })
+        .isLength({ min: 3 })
         .withMessage('Password too short.')
+        .bail()
+        .isLength({ max: 50 })
+        .withMessage('Password too long.')
         .escape(),
 
     (req, res, next) => {
@@ -30,8 +37,7 @@ exports.createUser = [
         if (!errors.isEmpty()) {
 
             // Got errors, send them back to frontend
-            let error = errors.array()[0];
-            console.log(error);
+            const error = errors.array()[0];
             res.status(406).send({ error: error.param, message: error.msg });
             return;
 
@@ -43,6 +49,56 @@ exports.createUser = [
                 role: 0
             });
             user.save();
+            res.sendStatus(200);
+        }
+    }
+]
+
+exports.loginUser = [
+    body('username')
+        .trim()
+        .isLength({ min: 3 })
+        .withMessage('Username too short.')
+        .isLength({ max: 50 })
+        .withMessage('Username too long.')
+        .bail()
+        .isAlphanumeric()
+        .withMessage('Only letters and numbers.')
+        .bail()
+        .escape(),
+
+    body('password')
+        .isLength({ min: 3 })
+        .withMessage('Password too short.')
+        .isLength({ max: 50 })
+        .withMessage('Password too long.')
+        .escape(),
+
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+
+            // Got errors, send them back to frontend
+
+            const error = errors.array()[0];
+            res.status(406).send({ error: error.param, message: error.msg });
+            return;
+
+        } else {
+            User.findOne({ username: req.body.username })
+                .exec((err, user) => {
+                    if (err) return res.sendStatus(400);
+                    if (!user) return res.status(404).send({ error: "username", message: "User not found." });
+
+                    if (req.body.password === user.password) {
+
+                        // All good, let's login
+
+                        res.sendStatus(200)
+                    } else {
+                        res.status(406).send({ error: "password", message: "Wrong password." });
+                    }
+                })
         }
     }
 ]
