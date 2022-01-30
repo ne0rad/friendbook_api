1ceea57a1cdbe06fcd6b8ee42451a4bd6a5e950d
ne0rad <usackasr@gmail.com>
2022-01-30 22:28:51 +0000

    Updated post model

--- models/post.js
+++ models/post.js
@@ -2,15 +2,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var PostSchema = new Schema({
-    author: { type: String, required: true, minlength: 1, maxlength: 100 },
+    author: { type: Schema.Types.ObjectId, ref: 'User' },
     title: { type: String, required: true, minlength: 1, maxlength: 100 },
     content: { type: String, required: true },
     date: { type: Date, required: true }
 });
-
-PostSchema.virtual('url')
-    .get(function () {
-        return '/posts/' + this._id;
-    });
 
 module.exports = mongoose.model('Post', PostSchema, 'posts');
