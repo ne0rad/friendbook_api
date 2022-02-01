38b6622ea486ccd2cda9ed2213dd324c409eb197
ne0rad <usackasr@gmail.com>
2022-02-01 17:27:26 +0000

    Routes are using token auth middleware

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-const { get_user } = require('../controllers/user');
+const { verify_token } = require('../controllers/token');
+const { get_my_info } = require('../controllers/user');
 
-router.get('/get_user', get_user);
+// Every route here must have a valid token
+router.use(verify_token);
+
+router.get('/get_my_info', get_my_info);
 
 module.exports = router;
