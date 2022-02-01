d49ab43c4425f03046b691d78d7a756f5bfef00c
ne0rad <usackasr@gmail.com>
2022-02-01 17:27:14 +0000

    Using token auth middleware

--- controllers/user.js
+++ controllers/user.js
@@ -1,10 +1,8 @@
 const User = require('../models/user');
-const jwt = require('jsonwebtoken');
 
-exports.get_user = (req, res, next) => {
-    const token = req.body.token;
-    const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
-    User.findOne({ _id: decoded._id })
+exports.get_my_info = (req, res, next) => {
+
+    User.findById(req.user._id)
         .exec((err, user) => {
             if (err) return next(err);
             return res.status(200).json({
