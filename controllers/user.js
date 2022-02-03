11eb33f6af6920eb69c2e866dfaaf4616009d8f3
ne0rad <usackasr@gmail.com>
2022-02-03 21:10:02 +0000

    Added update_my_info route

--- controllers/user.js
+++ controllers/user.js
@@ -4,7 +4,10 @@ exports.get_my_info = (req, res, next) => {
 
     User.findById(req.user._id)
         .exec((err, user) => {
+
             if (err) return next(err);
+            if (!user) return res.status(404).json({ message: 'User not found.' });
+
             return res.status(200).json({
                 username: user.username,
                 email: user.email || null,
@@ -12,4 +15,22 @@ exports.get_my_info = (req, res, next) => {
                 lastname: user.lastname || null,
             })
         })
+}
+
+exports.update_my_info = (req, res, next) => {
+    
+        User.findById(req.user._id)
+            .exec((err, user) => {
+
+                if(err) return next(err);
+                if(!user) return res.status(404).json({ message: 'User not found.' });
+
+                user.username = req.body.username;
+                user.email = req.body.email;
+                user.firstname = req.body.firstname;
+                user.lastname = req.body.lastname;
+                user.save();
+                
+                return res.status(200).json({ message: 'User updated.' });
+            });
 }
