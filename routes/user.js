4e6544619ed5ffa32d55197abd76a31328c92af2
ne0rad <usackasr@gmail.com>
2022-02-10 15:05:20 +0000

    Added GET /user/notifications

--- routes/user.js
+++ routes/user.js
@@ -2,12 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 const { verify_token } = require('../controllers/token');
-const { get_my_info, update_my_info } = require('../controllers/user');
+const { get_my_info, update_my_info, notifications } = require('../controllers/user');
 
 // Every route here must have a valid token
 router.use(verify_token);
 
 router.get('/get_my_info', get_my_info);
 router.post('/update_my_info', update_my_info);
+router.get('/notifications', notifications);
 
 module.exports = router;
