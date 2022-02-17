1949a4099c07825d2a8dddb1efa2917c51f350ef
ne0rad <usackasr@gmail.com>
2022-02-17 21:20:10 +0000

    Updated routes

--- routes/messages.js
+++ routes/messages.js
@@ -2,13 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 const { verify_token } = require('../controllers/token');
-const { send, user } = require('../controllers/messages');
+const { send, get_messages } = require('../controllers/messages');
 
 // Every route here must have a valid token
 router.use(verify_token);
 
 router.post('/send', send);
 
-router.get('/user', user);
+router.get('/get_messages', get_messages);
 
 module.exports = router;
