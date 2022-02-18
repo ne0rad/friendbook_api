184c75900a1a40e82ad97def722a5a1f0e6fbec6
ne0rad <usackasr@gmail.com>
2022-02-18 22:31:45 +0000

    Updated message model

--- models/message.js
+++ models/message.js
@@ -5,8 +5,7 @@ var MessageSchema = new Schema({
     chatroom: { type: Schema.Types.ObjectId, ref: 'Chatroom' },
     author: { type: Schema.Types.ObjectId, ref: 'User' },
     message: { type: String, required: true },
-    date: { type: Date, required: true, default: Date.now },
-    read: { type: Boolean, default: false }
+    date: { type: Date, required: true, default: Date.now }
 });
 
 module.exports = mongoose.model('Message', MessageSchema, 'messages');
