12b198c2d52278ffe9a2c384e9d7668c0c07732f
ne0rad <usackasr@gmail.com>
2022-02-19 20:37:34 +0000

    Updated routes

--- routes/messages.js
+++ routes/messages.js
@@ -2,13 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 const { verify_token } = require('../controllers/token');
-const { send, get_messages } = require('../controllers/messages');
+const { send, create_chatroom, chatroom_messages } = require('../controllers/messages');
 
 // Every route here must have a valid token
 router.use(verify_token);
 
 router.post('/send', send);
+router.post('/create_chatroom', create_chatroom);
 
-router.get('/get_messages', get_messages);
+router.get('/chatroom_messages', chatroom_messages);
 
 module.exports = router;
