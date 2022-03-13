6fb52627a57f28c7f0fe5350e37be42a40876885
ne0rad <usackasr@gmail.com>
2022-03-13 21:42:42 +0000

    If no callback sent will not reply

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 
 // Import controllers
 const { user_login, user_signup } = require("./controllers/auth");
-const { send_message, create_chat, join_chat, get_chats } = require("./controllers/messages");
+const { send_message, create_chat, join_chat, get_chats, read_chat } = require("./controllers/messages");
 
 // MongoDB connection
 const DEV_DB_URI = "mongodb://127.0.0.1:27017/friendbook";
@@ -16,7 +16,10 @@ io.on("connection", (socket) => {
   console.log('\x1b[32m%s\x1b[0m', `[socket.io] Client connected.`);
 
   function execute(func, data, callback) {
-    if (user && data && typeof callback === "function") {
+    if(typeof callback !== 'function' || typeof func !== 'function') {
+      return;
+    }
+    if (user && data) {
       func(user, data, (err, res) => {
         if (err) {
           callback(err);
@@ -25,7 +28,7 @@ io.on("connection", (socket) => {
         }
       }, socket);
     } else {
-      callback("You must be logged in to do that.");
+      callback("No user or data provided.");
     }
   }
 
@@ -57,6 +60,7 @@ io.on("connection", (socket) => {
   socket.on('create_chat', (data, callback) => execute(create_chat, data, callback));
   socket.on('join_chat', (data, callback) => execute(join_chat, data, callback));
   socket.on('get_chats', (data, callback) => execute(get_chats, data, callback));
+  socket.on('read_chat', (data, callback) => execute(read_chat, data, callback));
   // ----------------------------
   // End of commands for logged in users
 
