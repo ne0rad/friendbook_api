d543f7c3bb4e7873641c17385ff44629496e1a5d
ne0rad <usackasr@gmail.com>
2022-03-16 22:29:56 +0000

    Logging color change

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ io.on("connection", (socket) => {
   });
 
   socket.on('chat_leave', (data) => {
-    console.log('\x1b[32m%s\x1b[0m', `[socket.io] Client left ChatID: ${data.chatID} (${socket.id})`);
+    console.log('\x1b[31m%s\x1b[0m', `[socket.io] Client left ChatID: ${data.chatID} (${socket.id})`);
     socket.leave(data.chatID);
   });
 
