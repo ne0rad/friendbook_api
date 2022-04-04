8cf58176f3145d7d92d9c974d5afd33c492de856
ne0rad <usackasr@gmail.com>
2022-04-04 20:21:46 +0100

    Remaking server with TypeScript

--- socket.js
+++
@@ -1,9 +0,0 @@
-const socketIO = require('socket.io');
-const { server } = require('./server');
-
-module.exports = socketIO(server, {
-  cors: {
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
-    methods: ["GET", "POST"]
-  }
-});
