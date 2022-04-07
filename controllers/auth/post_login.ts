f5980af4d5650e0749d8d762247c62da75bccca4
ne0rad <usackasr@gmail.com>
2022-04-07 17:36:57 +0100

    Updated post_login

--- controllers/auth/post_login.ts
+++ controllers/auth/post_login.ts
@@ -11,14 +11,7 @@ export const post_login = [
     .isLength({ max: 20 })
     .withMessage("Username must be at most 20 characters long")
     .isAlphanumeric()
-    .withMessage("Username must be alphanumeric")
-    .custom((value) => {
-      return User.exists({ username: value }).then((user) => {
-        if (!user) {
-          return Promise.reject("Username doesn't exist");
-        }
-      });
-    }),
+    .withMessage("Username must be alphanumeric"),
   check("password")
     .isLength({ min: 3 })
     .withMessage("Password must be at least 3 characters long")
@@ -29,7 +22,7 @@ export const post_login = [
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      console.log('got errors')
+      console.log("got errors");
       const error = errors.array()[0];
       return res.status(401).json({ loc: error.param, msg: error.msg });
     }
